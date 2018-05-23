1f5d0ecd8cf5377b5b8c6766a15ba13a49ede3d6
Haakon Hukkelaas <haak-hu@hotmail.com>
2018-05-23 16:32:41 -0700

    add: life improvements + stat logging

--- constants.py
+++ constants.py
@@ -1,16 +1,8 @@
-# Indicates whether or not to use GPU
-USE_GPU = True
-
 # Directory to save models
 SAVE_DIR = "saved_models"
 # Directory to save plots
 PLOT_DIR = "plots"
-# Name of the model to be loaded
-SAVED_MODEL = "modelk.pt"
-# Interval between two stats saved
-DISPLAY_STEP = 450
+# Directory to save logs
+LOG_DIR = "logs"
 
-# Hyperparameters
-MAX_EPOCHS = 1000
-BATCH_SIZE = 128
 
