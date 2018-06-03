0318214c12610036b1b33b45bc0ad420a90b161b
ethanleet <ethanleet@qq.com>
2018-06-02 17:39:25 -0700

    add: reconstruction for smallNORB

--- constants.py
+++ constants.py
@@ -13,7 +13,7 @@ SMALL_NORB_PATH = os.path.join("datasets", "smallNORB")
 # Default values for command arguments
 DEFAULT_LEARNING_RATE = 0.001
 DEFAULT_ALPHA = 0.0005 # Scaling factor for reconstruction loss
-DEFAULT_DATASET = "mnist" # 'mnist', 'small_norb'
+DEFAULT_DATASET = "small_norb" # 'mnist', 'small_norb'
 DEFAULT_DECODER = "FC" # 'FC' or 'Conv'
 DEFAULT_BATCH_SIZE = 128
 DEFAULT_EPOCHS = 300 # DEFAULT_EPOCHS = 300
