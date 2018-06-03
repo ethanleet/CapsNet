fix: change softmax dimension for routing iteration

model.py
@@ -87,7 +87,8 @@ class ClassCapsules(nn.Module):
       b_ij = b_ij.cuda()
     
     for it in range(self.routing_iterations):
-      c_ij = functional.softmax(b_ij, dim=1) # Not sure if it should be dim=1
+      c_ij = functional.softmax(b_ij, dim=2) # Not sure if it should be dim=1
+
       c_ij = torch.cat([c_ij] * batch_size, dim=0).unsqueeze(4)
 
       s_j = (c_ij * u_hat).sum(dim=1, keepdim=True) + self.bias
@@ -153,8 +154,6 @@ class ConvReconstructionModule(nn.Module):
       nn.ConvTranspose2d(in_channels=10, out_channels=32, kernel_size=9, stride=2),
       nn.BatchNorm2d(32),
       nn.ReLU(),
-        
-      
       nn.ConvTranspose2d(in_channels=32, out_channels=64, kernel_size=9, stride=1),  
       nn.BatchNorm2d(64), 
       nn.ReLU(),
@@ -191,13 +190,14 @@ class CapsNet(nn.Module):
   
   def __init__(self,
                alpha=0.0005, # Alpha from the loss function
-               reconstruction_type = "FC"
+               reconstruction_type = "FC",
+               routing_iterations=3
               ):
     super(CapsNet, self).__init__()
     
     self.conv_layer = ConvLayer()
     self.primary_capsules = PrimaryCapules()
-    self.digit_caps = ClassCapsules()
+    self.digit_caps = ClassCapsules(routing_iterations=routing_iterations)
     if reconstruction_type == "FC":
         self.decoder = ReconstructionModule()
     else:

train.py
@@ -27,7 +27,7 @@ def transform_data(data,target,use_gpu):
 
 
 def main(opts):
-    capsnet = CapsNet(reconstruction_type=opts.decoder, alpha = opts.alpha)
+    capsnet = CapsNet(reconstruction_type=opts.decoder, alpha = opts.alpha, routing_iterations = opts.routing_iterations)
     if opts.use_gpu:
         capsnet.cuda()
     optimizer = torch.optim.Adam(capsnet.parameters(), lr=opts.learning_rate)