6978a58d6c8b07c412d1090271c77046be74d56e
Haakon Hukkelaas <haak-hu@hotmail.com>
2018-06-03 17:31:33 -0700

    f

--- data_loaders.py
+++ data_loaders.py
@@ -53,3 +53,25 @@ def load_small_norb(batch_size):
                                                shuffle=False)
     
     return train_loader, test_loader
+
+def load_cifar10(batch_size):
+    dataset_transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+            ])
+    train_dataset = datasets.CIFAR10('../data',
+                                    train=True,
+                                    download=True,
+                                    transform=dataset_transform)
+    test_dataset = datasets.CIFAR10('../data',
+                                    train=False,
+                                    download=True,
+                                    transform=dataset_transform)
+    
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset,
+                                              batch_size=batch_size,
+                                              shuffle=False)
+    return train_loader,test_loader
